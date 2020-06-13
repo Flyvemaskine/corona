0c93c79f65098421c1614bef774d5d0b4a450353
Chalie <crfederici@gmail.com>
2020-06-12 21:02:14 -0500

    add state tracking data file

--- data_prep_scripts/pull_testing.py
+++ data_prep_scripts/pull_testing.py
@@ -1,1 +1,12 @@
-# Code will go here
+#!/usr/bin/env python3
+
+import pandas as pd
+
+
+tracking_df = pd.read_csv("https://covidtracking.com/api/v1/states/daily.csv")
+tracking_df['date'] = pd.to_datetime(tracking_df['date'], format='%Y%m%d')
+
+tracking_df[['date', 'state', 'positive',
+             'negative', 'positiveIncrease',
+             'negativeIncrease','dataQualityGrade']] \
+    .to_csv("/Users/CharlesFederici/corona_python/data/testing.csv")
