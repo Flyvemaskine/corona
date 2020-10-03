f74bdcc88cb0d54232652e06c464e8b462ca7a32
Chalie <crfederici@gmail.com>
2020-10-02 19:06:52 -0500

    clean old folders

--- dash_app/data_prep_scripts/pull_manipulate_all_data.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-#echo "Pulling JHU: "$(date)
-python data_prep_scripts/pull_manipulate_jhu.py
-#echo "Pulling testing: "$(date)
-python data_prep_scripts/pull_testing.py
-# echo "Uploading to mongo: "$(date)
-python data_prep_scripts/create_app_plots.py
